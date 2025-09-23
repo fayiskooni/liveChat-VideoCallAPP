91a6358040b2941d4969fc2451ac362f389c02d9
fayiskooni <fayiskooni@gmail.com>
2025-09-23 11:52:58 +0530

    friend request endpoint completed

--- backend/src/server.js
+++ backend/src/server.js
@@ -3,6 +3,7 @@ import "dotenv/config";
 import cookieParser from "cookie-parser";
 
 import authRoutes from "./routes/auth.route.js";
+import userRoutes from "./routes/user.route.js";
 import { connectDB } from "./lib/db.js";
 
 const app = express();
