0e4b1b3c9e777ede3699aa71f14f6c4a5450f032
fayiskooni <fayiskooni@gmail.com>
2025-09-26 19:09:07 +0530

    Chat page Completed

--- frontend/src/pages/SignUpPage.jsx
+++ frontend/src/pages/SignUpPage.jsx
@@ -31,7 +31,7 @@ const SignUpPage = () => {
           <div className="mb-4 flex items-center justify-start gap-2">
             <ShipWheelIcon className="sixe-9 text-primary" />
             <span className="text-3xl font-bold font-mono bg-clip-text text-transparent bg-gradient-to-r from-primary to-secondary tracking-wider">
-              Streamify
+              Loomsy
             </span>
           </div>
           {/* Error message if any */}
@@ -48,7 +48,7 @@ const SignUpPage = () => {
                 <div>
                   <h2 className=" text-xl font-semibold">Create an Account</h2>
                   <p className=" text-sm opacity-70">
-                    Join Streamify and start your language learning adventure
+                    Join Loomsy and start your language learning adventure
                   </p>
                 </div>
                 <div className=" space-y-3">
